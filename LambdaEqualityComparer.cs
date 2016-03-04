2124fbdf09db21a30bd7b3eaa77e3eddde2f5f73
rbenjes <r.benjes@outlook.com>
2016-03-04 15:29:48 +0000

    Added files via upload

---
+++ LambdaEqualityComparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils
+{
+    public class LambdaEqualityComparer
+    {
+        public static LambdaEqualityComparer<T> Create<T>(Func<T, T, bool> comparer)
+        {
+            return new LambdaEqualityComparer<T>(comparer);
+        }
+    }
+    public class LambdaEqualityComparer<T> : EqualityComparer<T>
+    {
+        Func<T, T, bool> comparer;
+        public LambdaEqualityComparer(Func<T, T, bool> comparer)
+        {
+            this.comparer = comparer;
+        }
+        public override bool Equals(T x, T y)
+        {
+            return comparer(x, y);
+        }
+
+        public override int GetHashCode(T obj)
+        {
+            return 0;
+        }
+    }
+}
