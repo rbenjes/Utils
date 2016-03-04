d8619a53f234be22dd1e5c4bb83e61ef77932e54
rbenjes <r.benjes@outlook.com>
2016-03-04 14:34:56 +0000

    Added files via upload

---
+++ ObjectExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class ObjectExtensions
+    {
+        public static void DoWith<T>(this T source, Action<T> action) where T : class
+        {
+            if (source != null)
+                action(source);
+        }
+        public static TResult EvaluateIfNotNull<TResult, TSource>(this TSource source, Func<TSource, TResult> selector)
+        {
+            return EvaluateOrDefault(source, selector, () => default(TResult));
+        }
+        public static TResult EvaluateOrDefault<TResult, TSource>(this TSource source, Func<TSource, TResult> selector, Func<TResult> defaultSelector)
+        {
+            if (object.Equals(source, default(TSource)))
+                return defaultSelector();
+            return selector(source);
+        }
+    }
+}
