cc2d714f38a66ac51e77e4161441511d8578897e
rbenjes <r.benjes@outlook.com>
2016-03-04 14:38:29 +0000

    Added files via upload

--- ObjectExtensions.cs
+++ ObjectExtensions.cs
@@ -4,16 +4,16 @@ namespace Utils
 {
     public static class ObjectExtensions
     {
-        public static void DoWith<T>(this T source, Action<T> action) where T : class
+        public static void TryExe<T>(this T source, Action<T> action) where T : class
         {
             if (source != null)
                 action(source);
         }
-        public static TResult EvaluateIfNotNull<TResult, TSource>(this TSource source, Func<TSource, TResult> selector)
+        public static TResult TryEval<TResult, TSource>(this TSource source, Func<TSource, TResult> selector)
         {
-            return EvaluateOrDefault(source, selector, () => default(TResult));
+            return TryEvalOrDefault(source, selector, () => default(TResult));
         }
-        public static TResult EvaluateOrDefault<TResult, TSource>(this TSource source, Func<TSource, TResult> selector, Func<TResult> defaultSelector)
+        public static TResult TryEvalOrDefault<TResult, TSource>(this TSource source, Func<TSource, TResult> selector, Func<TResult> defaultSelector)
         {
             if (object.Equals(source, default(TSource)))
                 return defaultSelector();
